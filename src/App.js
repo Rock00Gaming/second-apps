be8e86ffd6ee4204cc0b10ec649b103a89f4169f
Rock00Gaming <mralokdj217@gmail.com>
2023-10-09 17:33:31 +0530

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 
 import React, { Component } from "react";
+import { NavBar } from "./components/Navbar";
 
 export default class App extends Component {
-  c = "Alok is creating";
   render() {
-    return <div>I am Creating my first class based component{this.c}</div>;
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
   }
 }
