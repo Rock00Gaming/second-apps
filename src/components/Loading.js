ce3f933c8255eea54c7ef7097cb5f500002e8e4f
Rock00Gaming <mralokdj217@gmail.com>
2023-10-09 23:04:03 +0530

    ok

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Loading extends Component {
   render() {
     return (
-      <div className="text-center my-3">
+      <div className="text-center">
         <div
           className="spinner-border text-center"
           style={{ width: "5rem", height: "5rem", margin: "auto" }}
