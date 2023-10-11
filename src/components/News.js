14ebe49b60c5dec27d4e7610831e164d012ab0f8
Rock00Gaming <mralokdj217@gmail.com>
2023-10-11 19:06:06 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -54,27 +54,6 @@ export class News extends Component {
     this.updateNews();
   };
   handleNextClick = async () => {
-    // if (
-    //   !(
-    //     this.state.page + 1 >
-    //     Math.ceil(this.state.totalResults / this.props.pageSize)
-    //   )
-    // ) {
-    //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country
-    //     }&category=${this.props.category
-    //     }&apiKey=f0e3c700a1f64c47b1d0dd7924d9fbdc&page=${this.state.page + 1
-    //     }&pageSize=${this.props.pageSize}`;
-    //   this.setState({ loading: true });
-    //   let data = await fetch(url);
-    //   let parsedData = await data.json();
-
-    //   console.log(parsedData);
-    //   this.setState({
-    //     page: this.state.page + 1,
-    //     articles: parsedData.articles,
-    //     loading: false,
-    //   });
-    // }
     this.setState({ page: this.state.page + 1 });
     this.updateNews();
   };
